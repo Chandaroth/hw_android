b4a1a0fdf524ffca3cd0c45169d298e12ad6b3c5
Chandaroth <naroth123@gmail.com>
2020-03-30 10:43:15 +0700

    update

--- MyApplication/app/src/main/java/com/example/firebasetesting/Login.java
+++ MyApplication/app/src/main/java/com/example/firebasetesting/Login.java
@@ -1,10 +1,9 @@
-package com.example.myapplication;
+package com.example.firebasetesting;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
--- MyApplication/app/src/main/java/com/example/firebasetesting/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/firebasetesting/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.firebasetesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
